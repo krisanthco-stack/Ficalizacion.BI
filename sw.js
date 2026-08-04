89e348c190190b1f39d8bc5b21665614a0526f5d
krisanthco-stack <krisanth.co@gmail.com>
2026-08-04 00:29:30 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fiscalizacion-bi-v19.1.0-build-1';
+const CACHE='fiscalizacion-bi-v20.0.0-build-1';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png',
